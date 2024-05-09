2691b4975f6a1d72e7ae7b9c1911352f6d58768b
tvago <tomas.vago@chghealthcare.com>
2024-05-09 14:04:09 +0200

    vsetko spolu

---
+++ SRS/SRS/SRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRSApp.swift
+//  SRS
+//
+//  Created by Tomáš Vago on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
